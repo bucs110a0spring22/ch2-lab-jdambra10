b52ea4c872999191156f2817b4a5e10c3730be8b
jdambra10 <98533715+jdambra10@users.noreply.github.com>
2022-02-14 02:14:00 +0000

    did lab

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import random
 
 #Part A
 weeks = 16
@@ -6,5 +7,15 @@ tuition = 6000
 cost_per_week = ((tuition / classes) / weeks)
 print("Cost per week:", cost_per_week)
 
+classes_per_week = 2
+print(classes_per_week, type(classes_per_week))
+cost_per_class = cost_per_week / classes_per_week
+print(cost_per_class, type(cost_per_class))
+print("Cost per class:", cost_per_class)
 
 #Part B
+#Random Agent generator for who to pick 
+valorant_agents = ["Cypher", "Killjoy", "Sage", "Chamber", "Viper", "Brimstone", "Astra", "Omen", "Sova", "Breach", "KAY/O", "Skye", "Phoenix", "Jett", "Reyna", "Raze", "Neon", "Yoru"]
+
+chosen_agent = random.choice(valorant_agents)
+print("For your next game, you should pick:", chosen_agent)
